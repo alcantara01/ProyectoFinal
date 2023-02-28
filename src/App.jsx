5cb4d2b2ba6a9f76919108063fc70a06dc4368cf
CristnaAT <alumno.668286@ies-azarquiel.es>
2023-02-28 18:53:07 +0100

    Commit Inicial (npm init vite + npm install)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h2>Componente Principal</h2>
+    </div>
+  )
+}
+
+export default App
